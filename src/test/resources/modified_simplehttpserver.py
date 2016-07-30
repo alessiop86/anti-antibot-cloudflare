516e7784c18c2b7e89adb97fbd5cab65d3d4b2aa
alessiop <alessio.piergiacomi@gmail.com>
2016-07-30 19:53:58 +0100

    test python server to manually see the behaviour of the script

--- src/test/resources/modified_simplehttpserver.py
+++ src/test/resources/modified_simplehttpserver.py
@@ -3,11 +3,13 @@ import logging
 
 cookieHeader = None
 
+
 class MyHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.cookieHeader = self.headers.get('Cookie')
-	print "Cookie:%s" % cookieHeader
+	    print self.headers
         SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
 
     def end_headers(self):
         self.send_my_headers()
@@ -15,8 +17,7 @@ class MyHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
 
     def send_my_headers(self):
-#        self.send_header('Cookie', self.cookieHeader)
-	self.send_header("Set-Cookie", "A=1")
+	self.send_header("Set-Cookie", "_cfuididi=0b59c51aca680614ad7d395510880dd7")
 
 if __name__ == '__main__':
     SimpleHTTPServer.test(HandlerClass=MyHTTPRequestHandler)
